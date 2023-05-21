565a344e08a2d9244b71c3a7313fac581071d528
Kerraz <mohamedreda.kerraz@gmail.com>
2023-05-21 20:58:23 +0200

    pr

--- controleur/controleur.class.php
+++ controleur/controleur.class.php
@@ -16,7 +16,8 @@
 		public static function verifConnexionClient ($email, $mdp)
 		{
 			$unClient = null;
-			$unClient = Controleur::$unModele->verifConnexionClient ($email, $mdp) ;
+			$unClient = Controleur::$unModele->verifConnexionClient ($email, $mdp);
+			print("test");
 			//on va le parser JSon
 				if($unClient == false){
 					return '[{"connect":"denied"}]';					
